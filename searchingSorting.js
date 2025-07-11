9d69e9f4a7795e0c97e5fac0bd2d6742108ab5ba
Yadnesh12 <yadneshbharambe123@gmail.com>
2025-07-11 19:32:12 +0530

    Day 11

--- searchingSorting.js
+++ searchingSorting.js
@@ -11,3 +11,25 @@ let arr = [2,4,3,1,5,0]
 console.log(linSort(arr,1))
 }
 
+// bineary search 
+{
+    let arr = [0,2,4,6,7,9,10]
+function binSearch(arr,num){
+    let initial = 0
+    let end = arr.length-1
+    let m 
+while( end >= initial){
+    m = Math.floor(initial+end)/2
+ if(arr[m] > num){
+    end = m-1
+ }
+ else if(arr[m] < num){
+    initial = m+1
+ }
+ else { return m }
+}
+return m
+}
+console.log(binSearch(arr,10))
+}
+
--- strings.js
+++ strings.js
@@ -5,20 +5,20 @@ function lastWord(s){
    let last = s.length-1
    let count = 0
     while(last>=0){
-        if(s[last] === " "){
+        if(s[last] === " "){  // helps to remove the blamk spaces from the end 
             last = last-1
         }
         else{
-            break
+            break // if there is any work it breakes and we geet the last number from where the letter stars
         }
     }
     while(last>=0){
-        if(s[last] !== " " ){
+        if(s[last] !== " " ){ // onces the blank spaces are removed it iterates over the word untill it finds a blank space
             count= count +1
             --last
         }
         else{
-            break
+            break // if it encounters any blank space again the loop breaks and we have the answer 
         }
     }
     return count
@@ -29,9 +29,44 @@ console.log(lastWord(s))}
 {
     let s = "  fly  me to the  moon  "
 function lastWord(s){
-  s = s.trim()
-  s = s.split(" ")
+  s = s.trim()  // trim the string from start and end 
+  s = s.split(" ") // split the string by " " into an array
 return s[s.length-1].length
 }
 console.log(lastWord(s))
+}
+
+// above solution with one loop 
+{
+let s = "  fly  me to the  moon  "
+function lastWord(s){
+  let n = s.length - 1 
+  let count =0
+  while(n>= 0){
+      if(s[n] !== " "){
+      count++
+      }
+      else if(count>0){ // if the count is greater than 0 the it will break from the loop as count will be increased only when it fount a word
+          break
+      }
+      n--
+  }
+return count
+}
+console.log(lastWord(s))
+}
+
+// Find word containing Character 
+{
+let words = ["aaa","asd","asddsd","abba","sdxc"]
+function findWord(words,x){
+    let newArr = []
+    for(let i = 0 ; i< words.length ; i++){
+        if(words[i].includes(x)){ // include method tells whether that variable is present in that string or not 
+            newArr.push(i) // if the variable is present in the string it pushes it to the new string 
+        }
+    }
+ return newArr  
+}
+console.log(findWord(words,"s"))
 }
