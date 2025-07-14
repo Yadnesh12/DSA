0d17b00b026a7f260f641e60f705a1bc93202946
Yadnesh12 <yadneshbharambe123@gmail.com>
2025-07-14 17:33:30 +0530

    Day 12 Strings

--- strings.js
+++ strings.js
@@ -1,6 +1,7 @@
 //calculate the size of last char in an string \
 // two loops method
-{let s = "  fly  me to the  moon  "
+{
+let s = "  fly  me to the  moon  "
 function lastWord(s){
    let last = s.length-1
    let count = 0
@@ -23,7 +24,8 @@ function lastWord(s){
     }
     return count
 }
-console.log(lastWord(s))}
+console.log(lastWord(s))
+}
 
 //Shortcut method for above solution 
 {
@@ -69,4 +71,98 @@ function findWord(words,x){
  return newArr  
 }
 console.log(findWord(words,"s"))
+}
+
+//find number of jewels present in tthe stones
+{
+let jewel = "aA"
+let stones = "aAAbbbbaBAB"
+let num = 0
+function jsort(j,s){
+    for(let i=0 ; i< j.length;i++){
+        for(let k = 0;k<s.length;k++){
+            if(j[i] === s[k]){
+                num++
+            }
+        }
+    }
+    return num
+}
+console.log(jsort(jewel,stones))
+}
+
+// same implementation of above using SET()
+{
+let jewel = "aA"
+let stones = "aAAbbbbaBAB"
+let jSet = new Set()  // creating the set 
+function jSort(jewel,stones){ //iterates over all the jewels to store it inside set as all jewels are unique and duplicates can be ignored 
+for(let i =0; i< jewel.length;i++){ //passing all the jewls to set in set all the duplicate values are ignored so every char will be present ones 
+    jSet.add(jewel[i]) 
+}
+let count = 0
+for(let j =0; j< stones.length;j++){ //iterates over all stones to find out number of jewels present in it
+    if(jSet.has(stones[j])){  //checks if the jSet matches the values that of stones if it match then count is increased by one else it is a stone and can be ignored
+        count++
+    }
+}
+return count  //return the count of array in the stones 
+}
+console.log(jSort(jewel,stones))
+}
+
+//fine the max number of vowels repeated and consonant repeted and return back the count 
+{
+    let letter = "successes"
+let countV = 0
+let countC = 0 
+let letMap = {}  // using the hashmap method to make it simple always use map while in similar condition when we need to find how many time a charactedr is repeating in the function 
+function vowCon(letter){
+    for(i=0;i<letter.length ; i++){
+        if(!letMap[letter[i]]){  // if the char is present in the Hashmap increase the count of that char by 1 
+            letMap[letter[i]] = 1
+        }
+        else{ 
+             ++letMap[letter[i]]  // if the char is not present in the hashmap add that char to the list and initialize it a count 1 
+        }
+    }
+    const vowels = ["a","e","i","o","u"]  // creating a new array of vowels to compare vowels present in the string
+    for(let i=0;i<letter.length ; i++){
+        if(vowels.includes(letter[i])){  // here we are checking whether the vowels are present in the main string 
+          countV =   Math.max(countV,letMap[letter[i]]) //if the vowels is present in the array than check the count of that char in the hash map and if the count is more than the countV then assign that value to countV else can be ignored 
+        }
+        else{
+         countC =   Math.max(countC,letMap[letter[i]]) //if the vowels are not present in the array than check the count of that char in the hash map and if the count is more than the countC then assign that value to countC else can be ignored 
+        }
+    }
+return countV+countC // return the count of vowels and consonant
+}
+console.log(vowCon(letter))
+}
+
+//Split a string in balanced strings 
+{
+let str = "RLRRLLRRLLRL"
+function balStr(s){
+    let rcount = 0
+    let lcount = 0
+    let strcount = 0
+    for(let i = 0; i < s.length ; i++){
+        // if(s[i] === "R"){
+        //     ++rcount
+        // }else if(s[i] === "L"){
+        //     ++lcount
+        // }
+        s[i] === 'R'? ++rcount : ++lcount // if the value is equal to r increment rcount else lcount
+        
+        if(rcount === lcount && rcount > 0){ //if both values are equal then increase str count whihc shows there are same number of L and R 
+            ++strcount
+            rcount = 0
+            lcount = 0
+        }
+        
+    }
+return strcount 
+}
+console.log(balStr(str))
 }
